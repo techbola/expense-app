3106c4b79fc6045ab332ea82c6de7ffb626dacb4
Adebola <adebola@poptin.com>
2023-09-18 19:08:19 +0100

    get all report endpoint, get single report endpoint, create a report endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,67 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { data, ReportType } from 'src/data';
+import { v4 as uuid } from 'uuid';
+// import { AppService } from './app.service';
 
-@Controller()
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+@Controller('report/:type')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
+  }
+
+  @Get(':id')
+  getReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.find(
+      (report) => report.type === reportType && report.id === id,
+    );
+  }
+
+  @Post()
+  createReport(
+    @Body() { amount, source }: { source: string; amount: number },
+    @Param('type') type: string,
+  ) {
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
+    };
+    data.report.push(newReport);
+    return newReport;
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'Updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'Deleted';
   }
 }
