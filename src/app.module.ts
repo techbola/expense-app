3287edcb0c8e9d20af28593adb07a8d7e03e24c5
Adebola <adebola@poptin.com>
2023-09-21 07:07:10 +0100

    refactor report module, summary module, dtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { SummaryModule } from './summary/summary.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [],
+  imports: [SummaryModule, ReportModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
--- src/dtos/report.dto.ts
+++ src/dtos/report.dto.ts
@@ -1,4 +1,12 @@
-import { IsNumber, IsPositive, IsString, IsNotEmpty, IsOptional } from 'class-validator';
+import {
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+} from 'class-validator';
+import { Exclude, Expose } from 'class-transformer';
+import { ReportType } from 'src/data';
 
 export class CreateReportDto {
   @IsNumber()
@@ -20,4 +28,27 @@ export class UpdateReportDto {
   @IsString()
   @IsNotEmpty()
   source: string;
+}
+
+export class ReportResponseDto {
+  id: string;
+  source: string;
+  amount: number;
+
+  @Exclude()
+  created_at: Date;
+
+  @Exclude()
+  updated_at: Date;
+
+  type: ReportType;
+
+  @Expose({ name: 'createdAt' })
+  transformCreatedAt() {
+    return this.created_at;
+  }
+
+  constructor(partial: Partial<ReportResponseDto>) {
+    Object.assign(this, partial);
+  }
 }
